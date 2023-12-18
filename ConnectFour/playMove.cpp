3631c326b6b2900e3d4c4504045ea8d52bad2cbe
Kyle Dyer <114958251+KyleBD@users.noreply.github.com>
2023-12-17 20:49:47 -0500

    Update playMove.cpp

--- ConnectFour/playMove.cpp
+++ ConnectFour/playMove.cpp
@@ -1,18 +1,3 @@
-//
-//  playMove.cpp
-//  Connect Four Game
-//
-//  Created by Tarek Abdelrahman on 2019-06-07.
-//  Modified by Tarek Abdelrahman on 2020-09-17.
-//  Modified by Salma Emara and Ethan Hugh on 2023-09-06
-//  Copyright © 2019-2020 Tarek Abdelrahman. All rights reserved.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-//
-//  ECE244 Student: write your code for playMove in this file
-
 #include "GameState.h"
 #include "globals.h"
 
@@ -20,8 +5,6 @@
 #include <string>
 
 using namespace std;
-
-// Function prototypes (if you need)
 bool in_bounds(int row, int col){
   if( (row < 0) || (row > 6) || (col < 0) || (col > 6)){
     return false;
@@ -146,9 +129,6 @@ void playMove(GameState& game_state) {
       return;
     }
   }
-  // Change turn if move is valid,
-  // Change gameOver
-  // Change winner
 }
 
 
