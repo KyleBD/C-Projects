73ff1a804d92f91b3d3b0036c3bb61cdb3d97e9b
Kyle Dyer <114958251+KyleBD@users.noreply.github.com>
2023-12-17 20:52:19 -0500

    Update GameState.cpp

--- ConnectFour/GameState.cpp
+++ ConnectFour/GameState.cpp
@@ -1,17 +1,3 @@
-//
-//  GameState.cpp
-//  Connect Four Game
-//
-//  Created by Tarek Abdelrahman on 2019-06-07.
-//  Modified by Tarek Abdelrahman on 2020-09-17.
-//  Modified by Salma Emara and Ethan Hugh on 2023-09-06.
-//  Copyright © 2019-2020 Tarek Abdelrahman. All rights reserved.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-//
-
 #include "GameState.h"
 #include "globals.h"
 
@@ -42,13 +28,10 @@ void GameState::set_selectedRow(int value){
       return;
     }
   }
-  //If the program reaches this point then the move will be not valid as the row is full
   set_moveValid(false);
   return;
 }
 
-  // Set the selected column in the range (in the range 0..boardSize - 1)
-  // An out of range value is ignored and the function just returns
 void GameState::set_selectedColumn(int value){
   if(value<0 || value > 6){
     set_moveValid(false);
@@ -65,39 +48,31 @@ int GameState::get_selectedRow(){
 
 }
 
-  // Return the selected column (in the range 0..boardSize - 1)
 int GameState::get_selectedColumn(){
     return selectedColumn;
 }
 
-
-  // Get the moveValid value
 bool GameState::get_moveValid(){
     return moveValid;
 }
 
-  // Set the moveValid variable to value
 void GameState::set_moveValid(bool value){
     moveValid = value;
     return;
 }
 
-  // Get the gameOver value
 bool GameState::get_gameOver(){
     return gameOver;
 }
 
-  // Set the gameOver variable to value
 void GameState::set_gameOver(bool value){
     gameOver = value;
 }
 
-  // Get the value of turn
 bool GameState::get_turn(){
     return turn;
 }
 
-  // Set the value of turn
 void GameState::set_turn(bool value){
     if(value == true){
         turn = false;
@@ -109,25 +84,18 @@ void GameState::set_turn(bool value){
     }
 }
 
-  // Get the value of winner
 int GameState::get_winner(){
     return winner;
 }
 
-  // Set the value of winner;
 void GameState::set_winner(int value){
     winner = value;
 }
   
-  // Get the game board value at the board location at row and col
-  // This method checks that row, col and value are in range/valid
-  // and if not it returns Empty
 int GameState::get_gameBoard(int row, int col){
     if((row > boardSize-1) || (row  < 0) || (col > boardSize -1) || (col < 0)){
         return Empty;
     }
-    //cout << gameBoard[row][col] << endl;
-    //cout << row << col;
     return gameBoard[row][col];
 }
 
